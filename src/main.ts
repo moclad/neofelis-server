a7c903c990cf51ec32dcc38845035d3cbedad1b8
Beno <moclad@hotmail.com>
2022-12-27 16:31:58 +0100

    set global prefix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1');
   app.enableCors();
 
   app.useGlobalPipes(
